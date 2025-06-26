7851800f2474284fc346d50e025a3bc5f31a82b6
Sourabh Kondapaka <sourabhkondapaka@gmail.com>
2025-06-26 20:38:56 +0530

    Modified backend to handle multiple comms

--- src/jupyter_matlab_kernel/kernels/labextension_comm/communication.py
+++ src/jupyter_matlab_kernel/kernels/labextension_comm/communication.py
@@ -5,7 +5,7 @@ from ipykernel.comm import Comm
 
 class LabExtensionCommunication:
     def __init__(self, kernel):
-        self.comm = None
+        self.comms = {}
         self.kernel = kernel
         self.log = kernel.log
 
@@ -17,21 +17,25 @@ class LabExtensionCommunication:
         self.log.debug(
             f"Received comm_open message with id: {comm_id} and target_name: {target_name}"
         )
-        self.comm = Comm(comm_id=comm_id, primary=False, target_name=target_name)
+        comm = Comm(comm_id=comm_id, primary=False, target_name=target_name)
+        self.comms[comm_id] = comm
         self.log.info("Successfully created communication channel with labextension")
 
     async def comm_msg(self, stream, ident, msg):
         """Handler to execute when labextension sends a message with 'comm_msg' type."""
-        if not self.comm:
-            self.log.error(
-                "Received comm_msg but no communication channel is available"
-            )
-            raise Exception("No Communcation channel available")
 
         content = msg["content"]
         data = content["data"]
         action_type = data["action"]
         data = data["data"]
+        comm_id = content["comm_id"]
+        comm = self.comms.get(comm_id)
+
+        if not comm:
+            self.log.error(
+                "Received comm_msg but no communication channel is available"
+            )
+            raise Exception("No Communcation channel available")
 
         self.log.debug(
             f"Received action_type:{action_type} with data:{data} from the lab extension"
@@ -41,10 +45,11 @@ class LabExtensionCommunication:
         """Handler to execute when labextension sends a message with 'comm_close' type."""
         content = msg["content"]
         comm_id = content["comm_id"]
+        comm = self.comms.get(comm_id)
 
-        if self.comm and self.comm.comm_id == comm_id:
+        if comm:
             self.log.info(f"Comm closed with id: {comm_id}")
-            self.comm = None
+            del self.comms[comm_id]
 
         else:
             self.log.warning(f"Attempted to close unknown comm_id: {comm_id}")
--- src/jupyter_matlab_labextension/src/plugins/matlabCommunication.ts
+++ src/jupyter_matlab_labextension/src/plugins/matlabCommunication.ts
@@ -1,20 +1,20 @@
 // Copyright 2025 The MathWorks, Inc.
 
 import {
-    JupyterFrontEnd,
-    JupyterFrontEndPlugin
-} from '@jupyterlab/application';
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin,
+} from "@jupyterlab/application";
 
-import { DocumentRegistry } from '@jupyterlab/docregistry';
+import { DocumentRegistry } from "@jupyterlab/docregistry";
 import {
-    INotebookModel,
-    INotebookTracker,
-    NotebookPanel
-} from '@jupyterlab/notebook';
-import { KernelMessage } from '@jupyterlab/services';
-import { JSONObject, JSONValue, Token } from '@lumino/coreutils';
-import { DisposableDelegate } from '@lumino/disposable';
-import { NotebookInfo } from '../utils/notebook';
+  INotebookModel,
+  INotebookTracker,
+  NotebookPanel,
+} from "@jupyterlab/notebook";
+import { KernelMessage } from "@jupyterlab/services";
+import { JSONObject, JSONValue, Token } from "@lumino/coreutils";
+import { DisposableDelegate } from "@lumino/disposable";
+import { NotebookInfo } from "../utils/notebook";
 
 // Add more action types as needed
 type CommunicationData = {
@@ -24,7 +24,7 @@ type CommunicationData = {
 
 // A unique token for the comm service
 export const CommunicationService = new Token<any>(
-    '@mathworks/MatlabCommPlugin'
+  "@mathworks/MatlabCommPlugin"
 );
 
 export interface ICommunicationChannel {
@@ -50,124 +50,126 @@ export interface ICommunicationService {
 }
 
 export class MatlabCommunicationExtension
-implements
+  implements
     DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel>,
-    ICommunicationService {
-    private _comms = new Map<string, ICommunicationChannel>();
-    createNew (
-        panel: NotebookPanel,
-        context: DocumentRegistry.IContext<INotebookModel>
-    ): DisposableDelegate {
-        panel.sessionContext.ready
-            .then(async () => {
-                const kernel = panel.sessionContext.session?.kernel;
-                // If kernel is available, create channel and set up listeners.
-                if (!kernel) {
-                    console.error("Kernel not ready! Can't create communication channel");
-                    return new DisposableDelegate(() => {});
-                }
-
-                const notebookInfo = new NotebookInfo();
-                await notebookInfo.update(panel);
-
-                if (!notebookInfo.isMatlabNotebook()) {
-                    console.debug('Not a MATLAB notebook, skipping communication setup');
-                    return new DisposableDelegate(() => {});
-                }
-
-                console.log('MATLAB Communication plugin activated for ', panel.id);
-
-                // Create a unique channel name for this notebook
-                const channelName = 'matlab_comm_' + panel.id
-                console.log(
-                    'Attempting to create communication with the kernel using channel name',
-                    channelName
-                );
-                const comm = kernel.createComm(channelName);
-
-                try {
-                    await comm.open().done;
-                    console.debug('Communication channel opened successfully');
-                } catch (error) {
-                    console.error('Error opening communication channel', error);
-                    return new DisposableDelegate(() => {});
-                }
-
-                // comm.open()
-                //     .done.then(() => {
-                //         console.debug('Communication channel opened successfully');
-                //     })
-                //     .catch((error) => {
-                //         console.log("\n\n\n Hello world asdf \n\n\n")
-
-                //         console.error('Error opening communication channel', error);
-
-                //     });
-                
-                
-                
-                
-
-                    // Listen for messages from the kernel
-                comm.onMsg = (msg: KernelMessage.ICommMsgMsg) => {
-                    const data = msg.content.data as CommunicationData;
-                    console.debug('Recieved data from kernel: ', data);
-                };
-
-                // Handle comm close
-                comm.onClose = (msg) => {
-                    console.debug('Comm closed:', msg);
-                };
-
-                this._comms.set(panel.id, comm);
-                console.log('Communication channel created with ID: ', comm.commId, ' and target name ', comm.targetName);
-
-                // Clean up when notebook is disposed
-                panel.disposed.connect(() => {
-                    this._comms.delete(panel.id);
-                });
-            })
-            .catch((error) => {
-                console.error('Notebook panel was not ready', error);
-            });
-
-        return new DisposableDelegate(() => {
-            this._comms.get(panel.id)?.close();
-            this._comms.delete(panel.id);
-        });
-    }
-
-    getComm (notebookId: string): ICommunicationChannel {
-        const commChannel = this._comms.get(notebookId);
-        if (!commChannel) {
-            throw new Error(
-                `No communication channel found for notebook ID: ${notebookId}`
-            );
+    ICommunicationService
+{
+  private _comms = new Map<string, ICommunicationChannel>();
+  createNew(
+    panel: NotebookPanel,
+    context: DocumentRegistry.IContext<INotebookModel>
+  ): DisposableDelegate {
+    panel.sessionContext.ready
+      .then(async () => {
+        const kernel = panel.sessionContext.session?.kernel;
+        // If kernel is available, create channel and set up listeners.
+        if (!kernel) {
+          console.error("Kernel not ready! Can't create communication channel");
+          return new DisposableDelegate(() => {});
         }
-        return commChannel;
-    }
 
-    deleteComms (): void {
-        this._comms.clear();
+        const notebookInfo = new NotebookInfo();
+        await notebookInfo.update(panel);
+
+        if (!notebookInfo.isMatlabNotebook()) {
+          console.debug("Not a MATLAB notebook, skipping communication setup");
+          return new DisposableDelegate(() => {});
+        }
+
+        console.log("MATLAB Communication plugin activated for ", panel.id);
+
+        // Create a unique channel name for this notebook
+        const channelName = "matlab_comm_" + panel.id;
+        console.log(
+          "Attempting to create communication with the kernel using channel name",
+          channelName
+        );
+        const comm = kernel.createComm(channelName);
+
+        try {
+          await comm.open().done;
+          console.debug("Communication channel opened successfully");
+        } catch (error) {
+          console.error("Error opening communication channel", error);
+          return new DisposableDelegate(() => {});
+        }
+
+        // comm.open()
+        //     .done.then(() => {
+        //         console.debug('Communication channel opened successfully');
+        //     })
+        //     .catch((error) => {
+        //         console.log("\n\n\n Hello world asdf \n\n\n")
+
+        //         console.error('Error opening communication channel', error);
+
+        //     });
+
+        // Listen for messages from the kernel
+        comm.onMsg = (msg: KernelMessage.ICommMsgMsg) => {
+          const data = msg.content.data as CommunicationData;
+          console.debug("Recieved data from kernel: ", data);
+        };
+
+        // Handle comm close
+        comm.onClose = (msg) => {
+          console.debug("Comm closed:", msg);
+        };
+
+        this._comms.set(panel.id, comm);
+        console.log(
+          "Communication channel created with ID: ",
+          comm.commId,
+          " and target name ",
+          comm.targetName
+        );
+
+        // Clean up when notebook is disposed
+        panel.disposed.connect(() => {
+          this._comms.delete(panel.id);
+        });
+      })
+      .catch((error) => {
+        console.error("Notebook panel was not ready", error);
+      });
+
+    return new DisposableDelegate(() => {
+      this._comms.get(panel.id)?.close();
+      this._comms.delete(panel.id);
+    });
+  }
+
+  getComm(notebookId: string): ICommunicationChannel {
+    const commChannel = this._comms.get(notebookId);
+    if (!commChannel) {
+      throw new Error(
+        `No communication channel found for notebook ID: ${notebookId}`
+      );
     }
+    return commChannel;
+  }
+
+  deleteComms(): void {
+    this._comms.clear();
+  }
 }
 
 export const matlabCommPlugin: JupyterFrontEndPlugin<MatlabCommunicationExtension> =
   {
-      id: '@mathworks/matlabCommPlugin',
-      autoStart: true,
-      requires: [INotebookTracker],
-      provides: CommunicationService,
-      activate: (app: JupyterFrontEnd): MatlabCommunicationExtension => {
-          const matlabCommExtension = new MatlabCommunicationExtension();
-          app.docRegistry.addWidgetExtension('Notebook', matlabCommExtension);
+    id: "@mathworks/matlabCommPlugin",
+    autoStart: true,
+    requires: [INotebookTracker],
+    provides: CommunicationService,
+    activate: (app: JupyterFrontEnd): MatlabCommunicationExtension => {
+      const matlabCommExtension = new MatlabCommunicationExtension();
+      app.docRegistry.addWidgetExtension("Notebook", matlabCommExtension);
 
-          // Dispose resources created by this plugin when the page unloads.
-          // Need to add this seperately if the jupyterlab tab is closed directly.
-          window.addEventListener('beforeunload', () => {
-              matlabCommExtension.deleteComms();
-          });
+      // Dispose resources created by this plugin when the page unloads.
+      // Need to add this seperately if the jupyterlab tab is closed directly.
+      window.addEventListener("beforeunload", () => {
+        matlabCommExtension.deleteComms();
+      });
 
-          return matlabCommExtension;
-      }
+      return matlabCommExtension;
+    },
   };
